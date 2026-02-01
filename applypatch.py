2bf9c287a5ac5e911fedd150cbdfdf5e972f6984
Xenthio <thegooster88@gmail.com>
2026-02-02 08:40:12 +1100

    Fix patches for latest version

--- applypatch.py
+++ applypatch.py
@@ -39,7 +39,7 @@ patches64 = {
 'bin/win64/engine.dll': [
     [('4883ec480f10', 0), '31c0c3'], #c_frustumcull patches
     [('753cf30f10', 0), 'eb'], # brush entity backfaces
-    [('7e5244', 0), 'eb'], # world backfaces
+    [('0f8d????????4863c3', 0), 'e9d600000090'], # world backfaces
     [('753c498b4204', 0), 'eb'], # world backfaces
 ],
 'bin/win64/shaderapidx9.dll': [
@@ -208,3 +208,4 @@ for fname in patches:
 
 if problems:
     logwarn('\nWarning: Not all patches applied successfully')
+
